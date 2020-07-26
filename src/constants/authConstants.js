e3ba28b0276103ffa4c0ca437edf2692952876bd
sparshsamir <sparshsamir1993@gmail.com>
2020-07-26 15:49:19 -0400

    production changes

--- src/constants/authConstants.js
+++ src/constants/authConstants.js
@@ -5,7 +5,7 @@ let API_BASE_URL;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   API_BASE_URL = "http://localhost:5050/api/v1";
 } else {
-  API_BASE_URL = "http://localhost:1110/api/v1";
+  API_BASE_URL = "https://covid-management-backend-stage.herokuapp.com/api/v1";
 }
 
 export { API_BASE_URL };
